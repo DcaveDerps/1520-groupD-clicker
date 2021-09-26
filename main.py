016c972b47e302c8eeeb1078605013c168f7f049
DcaveDerps <djh118@pitt.edu>
2021-09-26 04:04:29 +0000

    added game.html with working counter

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
+@app.route('/static-files/index.html')
 def root():
     return flask.render_template('index.html', page_title='Login')
 
+@app.route('/game')
+@app.route('/game.html')
+def game():
+    return flask.render_template('game.html', page_title='Game')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
