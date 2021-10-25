0bd627c7e854eb794211b23e0b617024fcb21834
Jake-Bon <jacob.c.bonhomme@gmail.com>
2021-10-25 04:58:56 +0000

    Images half working

--- ds.py
+++ ds.py
@@ -1,5 +1,6 @@
 from google.cloud import datastore
 import datetime
+from google.cloud import storage
 
 """
 DATA STORAGE FUNCTIONS
@@ -25,6 +26,45 @@ def create_account_entity(username):
     key = ds_client.key('user_account', username) # id will be the given username
     return datastore.Entity(key)
 
+def create_img_entity(title,url):
+    ds_client = get_datastore_client()
+    key = ds_client.key('image',(url))
+    entity = datastore.Entity(key)
+    entity['url'] = url
+    entity['title']=title
+    ds_client.put(entity)
+
+def upload_img(title,img_file):
+    gcs_client = storage.Client()
+    bucket = gcs_client.get_bucket('1520-img-bucket')
+    blob = bucket.blob(img_file.filename)
+
+    c_type = img_file.content_type
+    blob.upload_from_string(img_file.read(),content_type=c_type)
+    create_img_entity(title,blob.public_url)
+
+
+def get_img_entity(filename):
+    ds_client = get_datastore_client()
+    key = ds_client.key('image',filename)
+    entity = ds_client.get(key)
+    if entity:
+        return entity
+    else:
+        return None
+
+def get_all_img_entities():
+    results = []
+
+    ds_client = get_datastore_client()
+    query = ds_client.query(kind='image')
+
+    for img in query.fetch():
+        results.append(img)
+    
+    return results
+
+
 # Stores/Updates the entity in the cloud database
 def update_entity(entity):
     ds_client = get_datastore_client()
@@ -42,6 +82,16 @@ def get_user_account(username):
     ds_client = get_datastore_client()
     key = ds_client.key('user_account', username)
     return ds_client.get(key)
+
+#Get leaderboard list
+def get_leaderboard():
+    results = []
+    client = get_datastore_client()
+    query = client.query(kind="user_account")
+    query.order = ['collectibles']
+    for entity in query.fetch():
+        results.append(entity)
+    return results
 
 # incredibly unsafe debug function
 def list_account_entities():
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google.cloud import datastore
 import ds
 import game
 import datetime
+import urllib
 
 app = flask.Flask(__name__)
 
@@ -29,76 +30,37 @@ def login():
 @app.route('/leaderboard')
 @app.route('/leaderboard.html')
 def leaderboard():
-    return flask.render_template('/leaderboard.html',page_title='Leaderboard')
+    player_list = ds.get_leaderboard()
+    return flask.render_template('/leaderboard.html',page_title='Leaderboard',players=player_list)
 
 @app.route('/marketplace')
 @app.route('/marketplace.html')
 def marketplace():
-    return flask.render_template('/marketplace.html', page_title='Marketplace')
+    return flask.render_template('/marketplace.html', page_title='Marketplace',status = None,items = ds.get_all_img_entities())
 
 @app.route('/create')
 @app.route('/create.html')
 def createAcc():
     return flask.render_template('/create.html',page_title='Create Account')
 
-"""
-DATA STORAGE
-"""
-
-"""
-# Returns an instance of the datastore Client for the application
-def get_datastore_client():
-    return datastore.Client('cs1520-group-d')
-
-# Creates and returns a new instance of an Entity of the given kind
-def create_entity(kind):
-
-    ds_client = get_datastore_client()
-
-    key = ds_client.key(kind) # ID will be generated automatically. Will be an int.
-    return datastore.Entity(key)
-
-# Creates and returns a new instance of an Entity that will hold account information
-def create_account_entity(username):
-
-    ds_client = get_datastore_client()
-
-    key = ds_client.key('user_account', username) # id will be the given username
-    return datastore.Entity(key)
-
-# Stores/Updates the entity in the cloud database
-def update_entity(entity):
-    ds_client = get_datastore_client()
-    ds_client.put(entity)
-
-# Removes an account entity from the cloud database
-def remove_account_entity(id):
-    ds_client = get_datastore_client()
-    key = ds_client.key('user_account', id)
-    ds_client.delete(key)
-
-# Returns the database Entity whose id is 'username'
-# Returns None if the Entity does not exist
-def get_user_account(username):
-    ds_client = get_datastore_client()
-    key = ds_client.key('user_account', username)
-    return ds_client.get(key)
-
-# incredibly unsafe debug function
-def list_account_entities():
-
-    ds_client = get_datastore_client()
-    query = ds_client.query(kind='user_account')
-
-    print('\nUSER ACCOUNTS:')
-
-    for acc in query.fetch():
-        print('running')
-        print(f"username: {acc['uname']} | password: {acc['password']} | collectibles: {acc['collectibles']} | cps: {acc['cps']}")
+@app.route('/upload-image', methods=['POST'])
+def handle_upload_img():
+    img_file = flask.request.files.get('img')
+    if not img_file:
+        return flask.render_template('/marketplace.html', page_title='Marketplace',status = 'No Image Selected',items = ds.get_all_img_entities())
+    title = flask.request.form.get('name')
+    if title=="":
+        return flask.render_template('/marketplace.html', page_title='Marketplace',status = 'No Name Provided',items = ds.get_all_img_entities())
     
-    print('-=-=-=-=-=-=-=-=-=-=-=-=-\n')
+    img_file.filename = (img_file.filename +" " +str(datetime.datetime.now()))
 
-"""
+    ds.upload_img(title,img_file)
+
+    return flask.render_template('/marketplace.html', page_title='Marketplace',status = 'Upload Success!',items = ds.get_all_img_entities())
+
+
+
+
 
 @app.route('/create-user', methods=['POST'])
 def handle_create_account_request():
